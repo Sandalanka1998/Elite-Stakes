f9c5c69a490e79947d1289beb5dbba2015560f64
K.M.K Sandalanka <134092886+Sandalanka1998@users.noreply.github.com>
2025-09-02 13:01:18 +0530

    Admin Automation

---
+++ Admin.cy.js
@@ -0,0 +1,16 @@
+describe('User Login',  () => {
+
+    it('Functions', () => {
+
+        cy.viewport(1920, 1080);
+        cy.visit("https://elite.longwapps.com/login");
+
+        cy.get('#email').type("test@test.com");
+        cy.wait(1000);
+        cy.get('#password').type("Roshel123");
+        cy.get(':nth-child(4)>:nth-child(4)').click(); // Click on the "Login" button
+        cy.wait(1000);
+
+
+    })    
+})
